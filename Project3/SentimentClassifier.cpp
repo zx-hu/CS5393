b368659575b363338e3a6bfb6b9538da72d6180c
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-10-29 20:23:28 -0500

    Update SentimentClassifier.cpp
    added processWord

--- Project3/SentimentClassifier.cpp
+++ Project3/SentimentClassifier.cpp
@@ -5,43 +5,56 @@
 #include <unordered_map>
 #include <vector>
 
-
+//constructor
 SentimentClassifier::SentimentClassifier(){
     positiveWords = std::unordered_map<DSString, size_t>();
     negativeWords = std::unordered_map<DSString, size_t>();
 }
 
+//preprocesses the word to make it easier on the classifer
+DSString SentimentClassifier::processWord(DSString w){
+    w=w.toLower();
+
+    //skip if word is too short
+    if(w.length() < 3){
+        return "";
+    }
+    
+    //remove loose quotes??
+    /*if(w.substring(0, 1) == "\""){
+        w = w.substring(1, w.length()-1);
+    }
+    if(w.substring(w.length()-2, 1) == "\""){
+        w = w.substring(0, w.length()-2);
+    }*/
+
+    //stem for ed and ing
+    if(w.substring(w.length()-2, 2) == "ed"){
+        w = w.substring(0, w.length() - 2);
+        
+    }else if(w.substring(w.length()-3, 3) == "ing"){
+        w = w.substring(0, w.length() - 3);
+    }
+
+    return w;
+}
+
+//trains the model by adding to the maps of positive and negative words
 void SentimentClassifier::train(DSString& text, DSString& sentiment){
     char delimiter = ' ';
     std::vector<DSString> words = text.split(delimiter); 
     for(auto w : words){
         try{
-            //skip if word is too short
-            if(w.length() < 3){
+            w = processWord(w);
+            
+            if(w.length() < 3) {
                 continue;
-            }
-            w=w.toLower();
-
-            //remove loose quotes??
-            /*if(w.toLower().substring(0, 1) == "\""){
-                w = w.substring(1, w.length()-1);
-            }
-            if(w.toLower().substring(w.length()-2, 1) == "\""){
-                w = w.substring(0, w.length()-2);
-            }*/
-
-            //stem for ed and ing
-            if(w.substring(w.length()-2, 2) == "ed"){
-                w = w.substring(0, w.length() - 2);
-                
-            }else if(w.substring(w.length()-3, 3) == "ing"){
-                w = w.substring(0, w.length() - 3);
             }
 
             if(sentiment == "4"){
-                positiveWords[w.toLower()]++;
+                positiveWords[w]++;
             }else if(sentiment == "0"){
-                negativeWords[w.toLower()]++;
+                negativeWords[w]++;
             }
         }catch(const std::exception& e){
             //std::cerr << "Error processing word: " << w << " - " << e.what() << "\n";
@@ -50,6 +63,7 @@ void SentimentClassifier::train(DSString& text, DSString& sentiment){
     }
 }
 
+//predicts the sentiment of a text based on the existing positive and negative word sets
 int SentimentClassifier::predict(DSString& text){
     size_t positive = 0;
     size_t negative = 0;
@@ -59,22 +73,10 @@ int SentimentClassifier::predict(DSString& text){
     for(auto w : words){
         try{
             
-            w = w.toLower();
+            w = processWord(w);
 
-            //remove loose quotes??
-            if(w.substring(0, 1) == "\""){
-                w = w.substring(1, w.length()-1);
-            }
-            if(w.substring(w.length()-2, 1) == "\""){
-                w = w.substring(0, w.length()-2);
-            }
-
-            //stem for ed and ing
-            if(w.substring(w.length()-2, 2) == "ed"){
-                w = w.substring(0, w.length() - 2);
-                
-            }else if(w.substring(w.length()-3, 3) == "ing"){
-                w = w.substring(0, w.length() - 3);
+            if(w.length() < 3) {
+                continue;
             }
 
             auto posIt = positiveWords.find(w);
