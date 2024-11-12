3c11a1ea2a4e40e2b60ef423167089f2d1db2715
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-11-11 21:46:48 -0600

    Update AVLTree.h

--- Project4/AVLTree.h
+++ Project4/AVLTree.h
@@ -9,23 +9,22 @@
 
 struct Node {
     std::string key;
-    std::vector<std::pair<rapidjson::Document*, int> > value;
+    std::vector<std::pair<std::string, int> > value;
     int height;
     Node* left;
     Node* right;
-    Node(std::string k, rapidjson::Document* docu);
-    ~Node();
+    Node(std::string k, std::string file);
 };
 
 int getHeight(Node* node);
 int getBalanceFactor(Node* node);
 Node* rotateRight(Node* y);
 Node* rotateLeft(Node* x);
-Node* insert(Node* node, std::string key, rapidjson::Document* docu);
+Node* insert(Node* node, std::string key, std::string file_path);
 void inOrderTraversal(Node* node);
 void saveTree(Node* node, std::ofstream& outFile);
 Node* loadTree(Node* node, std::ifstream& inFile);
-std::vector<std::pair<rapidjson::Document*, int> >* find(Node* root, const std::string key);
+std::vector<std::pair<std::string, int> > find(Node* root, const std::string key);
 
 
 
