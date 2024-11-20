e5a53411245e09264bc3614bbf0e1e134792a2c6
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-11-19 20:33:29 -0600

    Update QueryProcessor.cpp

--- Project4/QueryProcessor.cpp
+++ Project4/QueryProcessor.cpp
@@ -1,12 +1,10 @@
 #include "QueryProcessor.h"
 #include "porter2_stemmer-master/porter2_stemmer.h"
 #include <iostream>
+#include <unordered_set>
 #include <sstream>
 
 QueryProcessor::QueryProcessor(){
-    wordMap = nullptr;
-    personMap = nullptr;
-    orgMap = nullptr;
 }
 
 // Comparator to sort pairs by value in descending order
@@ -14,66 +12,45 @@ bool compareByValue(const std::pair<std::string, int>& a, const std::pair<std::s
     return a.second > b.second; // Greater to less
 }
 
+//outer join maps
 void mergeMaps(std::unordered_map<std::string, int>& map1, std::unordered_map<std::string, int>& map2){
     for(const auto& pair : map2){
         map1[pair.first] += pair.second;  
     }
 }
 
+//remove map2 from map1
 void mergeAndRemoveMaps(std::unordered_map<std::string, int>& map1, std::unordered_map<std::string, int>& map2){
     for(const auto& pair : map2){
         map1.erase(pair.first);
     }
 }
 
+//inner join maps
 void mergeAndSumMaps(std::unordered_map<std::string, int>& map1, std::unordered_map<std::string, int>& map2){
+    std::unordered_set<std::string> keys2;
     for(const auto& pair : map2){
+        keys2.insert(pair.first);
+        
         const std::string& key = pair.first;
-
-        auto it = map1.find(key);
+        auto it = map2.find(key);
         if(it != map1.end()){
             map1[key] += pair.second;
         }
     }
 
-    for(const auto& pair : map1){
-        const std::string& key = pair.first;
-
-        auto it = map2.find(key);
-        if(it == map2.end()){
-            map1.erase(key);
+    // Remove keys from map1 that are not in map2
+    for (auto it = map1.begin(); it != map1.end();) {
+        if (keys2.find(it->first) == keys2.end()) {
+            it = map1.erase(it);  // Erase and move to the next item
+        } else {
+            it++;
         }
     }
 }
 
 
-void QueryProcessor::loadAVLTrees(std::string wordMap_file, std::string personMap_file, std::string orgMap_file){
-    std::ifstream wordMapFile(wordMap_file);
-    if(wordMapFile.is_open()){
-        wordMap = loadTree(wordMap, wordMapFile);
-        wordMapFile.close();
-    }else{
-        std::cerr << "Failed to open file";
-    }
-
-    std::ifstream personMapFile(personMap_file);
-    if(personMapFile.is_open()){
-        personMap = loadTree(personMap, personMapFile);
-        personMapFile.close();
-    }else{
-        std::cerr << "Failed to open file";
-    }
-
-    std::ifstream orgMapFile(orgMap_file);
-    if(orgMapFile.is_open()){
-        orgMap = loadTree(orgMap, orgMapFile);
-        orgMapFile.close();
-    }else{
-        std::cerr << "Failed to open file";
-    }
-} 
-
-std::vector<std::pair<std::string,int> > QueryProcessor::search(std::string input){
+std::vector<std::pair<std::string,int> > QueryProcessor::search(std::string input, Node* wordMap, Node* personMap, Node* orgMap){
     std::stringstream ss(input);
     std::string word;
     std::unordered_map<std::string, int> results;
@@ -84,6 +61,11 @@ std::vector<std::pair<std::string,int> > QueryProcessor::search(std::string inpu
             //can a search have -ORG: ???
             //docu CANNOT contain this word
             word = word.substr(1); //cut off -
+            Porter2Stemmer::trim(word);
+            Porter2Stemmer::stem(word);
+            if(word.length()<3){
+                continue;
+            }
             single_result = find(wordMap, word);
             mergeMaps(removeResults, single_result);
             continue;
@@ -98,7 +80,6 @@ std::vector<std::pair<std::string,int> > QueryProcessor::search(std::string inpu
             single_result = find(personMap, word);
 
         }else{
-            //PROCESSWORD!!!
             Porter2Stemmer::trim(word);
             Porter2Stemmer::stem(word);
             if(word.length()<3){
@@ -112,10 +93,11 @@ std::vector<std::pair<std::string,int> > QueryProcessor::search(std::string inpu
             mergeAndSumMaps(results, single_result);
         }
     }
+    //remove search results with the -
     mergeAndRemoveMaps(results, removeResults);
     //put into vector for sorting
     std::vector<std::pair<std::string,int> >ret(results.begin(), results.end());
-
+    //sort
     std::sort(ret.begin(), ret.end(), compareByValue);
 
     return ret;
