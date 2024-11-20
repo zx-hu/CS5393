15598ae364f5f63af8d91f704722c1c8b4afafb6
zx-hu <167228682+zx-hu@users.noreply.github.com>
2024-11-19 20:30:06 -0600

    Update DocumentParser.h

--- Project4/DocumentParser.h
+++ Project4/DocumentParser.h
@@ -6,16 +6,11 @@
 
 class DocumentParser{
     private:
-        Node* wordMap;
-        Node* personMap;
-        Node* orgMap;
-        
-        std::string processWord(std::string word);
     public:
         DocumentParser();
-        void parseJsonFile(std::string file_path);
-        void process_folder(std::string folder_path);
-        void saveAVLTrees(std::string wordMap_file, std::string personMap_file, std::string orgMap_file);
+        void parseJsonFile(std::string file_path, Node*& wordMap, Node*& personMap, Node*& orgMap);
+        void process_folder(std::string folder_path, Node*& wordMap, Node*& personMap, Node*& orgMap);
+
 };
 
 #endif
